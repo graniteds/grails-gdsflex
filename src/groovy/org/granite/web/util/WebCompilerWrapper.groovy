47cff944511d21f32aa2b8288ce3a86d77a24996
agile.guo <agile.guo@832c1c66-4827-0410-8465-ccb17913cd09>
2009-04-16 08:03:32 +0000

    change WebCompile to WebCompilerWrapper and use force=true

--- src/groovy/org/granite/web/util/WebCompilerWrapper.groovy
+++ src/groovy/org/granite/web/util/WebCompilerWrapper.groovy
@@ -4,20 +4,21 @@ import org.granite.webcompiler.WebCompiler
 import org.granite.webcompiler.WebCompilerException
 import org.granite.webcompiler.WebCompilerType
 
-public class WebCompile {
+public class WebCompilerWrapper {
     private static WebCompiler webCompiler = WebCompiler.getInstance()
-    
+    private static def appXmlList = []
     static def init(basePath) {
         webCompiler.init(basePath)
     }
-
+    
     static def compile(sourceDir,appName) {
-        File root = new File(sourceDir)
-        def appXmlList = []
-        root.eachFileRecurse{ file->
-            if(file.name.endsWith(".mxml") && 
-                   file.text.indexOf("<mx:Application ") != -1 ) {
-                appXmlList.add(file)
+        if(appXmlList.size()==0) {
+            File root = new File(sourceDir)
+            root.eachFileRecurse{ file->
+                if(file.name.endsWith(".mxml") && 
+                file.text.indexOf("<mx:Application ") != -1 ) {
+                    appXmlList.add(file)
+                }
             }
         }
         appXmlList.each { file->
@@ -28,8 +29,8 @@ public class WebCompile {
                     swfDir.mkdirs()
                 }
                 webCompiler.compileMxmlFile(file, 
-                                            new File(swfDir,file.name.replaceAll("mxml\$","swf")),
-                                            false,WebCompilerType.application,"/${appName}")
+                        new File(swfDir,file.name.replaceAll("mxml\$","swf")),
+                        true,WebCompilerType.application,"/${appName}")
             }catch(WebCompilerException ex) {
             }
         }
