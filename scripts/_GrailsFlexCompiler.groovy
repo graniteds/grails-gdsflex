cf7e4c94b6d1b30a131552de883a92698c17db37
agile.guo <agile.guo@832c1c66-4827-0410-8465-ccb17913cd09>
2009-07-02 04:34:07 +0000

    restore the autoCompile

--- scripts/_GrailsFlexCompiler.groovy
+++ scripts/_GrailsFlexCompiler.groovy
@@ -1,71 +1,19 @@
 import grails.util.Environment
 
-import org.granite.webcompiler.WebCompiler
-import org.granite.webcompiler.WebCompilerType
+import org.granite.web.util.WebCompilerWrapper
 
 Ant.property(environment:"env")
 grailsHome = Ant.antProject.properties."env.GRAILS_HOME"
 
 includeTargets << grailsScript("_GrailsCompile")
 def configureFlexCompilation() {
-	rootLoader.addURL(new File(classesDirPath).toURI().toURL())
-    new File("${gdsflexPluginDir}/scripts/lib/compile").listFiles().each {
-    	rootLoader?.addURL(it.toURI().toURL())
-    }
-	WebCompiler webCompiler = WebCompiler.getInstance()
-    webCompiler.init("${basedir}/web-app/WEB-INF")
-	return webCompiler
-}
-
-def compile(webCompiler,sourceDir,appName,mxmlFiles) {
-    def appXmlList = []
-    if(!mxmlFiles) {
-        File root = new File(sourceDir)
-        root.eachFileRecurse{ file->
-            checkXmlList(appXmlList,file)
-        }
-    }else {
-        mxmlFiles.each { 
-        	def fileName = it
-			if(!fileName.endsWith(".mxml")) {
-				fileName += ".mxml"
-			}
-        	checkXmlList(appXmlList,new File(sourceDir+"/"+fileName)) 
-			}
-    }
-    appXmlList.each { appXml->
-        def file = appXml.file
-        try {
-            String sep = File.separator=="\\"?"\\\\":File.separator
-            File swfDir = new File(file.parent.replaceAll("views${sep}flex","views${sep}swf"))
-            if(!swfDir.exists()) {
-                swfDir.mkdirs()
-            }
-            println "compiling file " + file.name
-            webCompiler.compileMxmlFile(file, 
-                    new File(swfDir,file.name.replaceAll("mxml\$","swf")),
-                    true,appXml.type,appName)
-        }catch(Exception ex) {
-            println "error during compilation " + ex.getMessage()
-        }
-        println file.name + " compilation ended at: " + new Date()
-    }
-}
-
-private def checkXmlList(appXmlList,file) {
-    if(file.name.endsWith(".mxml")) {
-        String content = file.text
-        if(content.indexOf("</mx:Application>") != -1 ) {
-            appXmlList.add([file:file,type:WebCompilerType.application])
-        }else if(content.indexOf("</mx:Module>")!=-1){
-            appXmlList.add([file:file,type:WebCompilerType.application])
-        }
-    }
+	//rootLoader.addURL(new File(classesDirPath).toURI().toURL())
+    WebCompilerWrapper.init("${basedir}/web-app/WEB-INF")
 }
 
 target(flexCompile: "Compile the flex file to swf file") {
     depends(compilePlugins)
-    WebCompiler webCompiler = configureFlexCompilation()
+    configureFlexCompilation()
     if(Environment.current==Environment.DEVELOPMENT) {
         Ant.copy(tofile: "${basedir}/web-app/WEB-INF/flex/flex-config.xml",
                 file:"${gdsflexPluginDir}/src/flex/flex-config-debug.xml")
@@ -74,5 +22,5 @@ target(flexCompile: "Compile the flex file to swf file") {
                 file:"${gdsflexPluginDir}/src/flex/flex-config-release.xml",
                 overwrite: true)
     }
-    compile(webCompiler,"${basedir}/grails-app/views/flex",grailsAppName,argsMap["params"])
+    WebCompilerWrapper.compile("${basedir}/grails-app/views/flex",grailsAppName,argsMap["params"])
 }
---
+++ src/groovy/org/granite/web/util/WebCompilerWrapper.groovy
@@ -0,0 +1,61 @@
+package org.granite.web.util
+
+import org.granite.webcompiler.WebCompiler
+import org.granite.webcompiler.WebCompilerException
+import org.granite.webcompiler.WebCompilerType
+
+public class WebCompilerWrapper {
+    private static WebCompiler webCompiler = WebCompiler.getInstance()
+    
+    static def init(basePath) {
+        webCompiler.init(basePath)
+    }
+        
+     static def compile(sourceDir,appName,mxmlFiles=null) {
+        def appXmlList = []
+        if(!mxmlFiles) {
+            File root = new File(sourceDir)
+            root.eachFileRecurse{ file->
+                checkXmlList(appXmlList,file)
+            }
+        }else {
+            mxmlFiles.each { 
+                def fileName = it
+                if(!fileName.endsWith(".mxml")) {
+                    fileName += ".mxml"
+                }
+                checkXmlList(appXmlList,new File(sourceDir+"/"+fileName)) 
+            }
+        }
+        appXmlList.each { appXml->
+            def file = appXml.file
+            try {
+                String sep = File.separator=="\\"?"\\\\":File.separator
+                File swfDir = new File(file.parent.replaceAll("views${sep}flex","views${sep}swf"))
+                if(!swfDir.exists()) {
+                    swfDir.mkdirs()
+                }
+                println "compiling file " + file.name
+                webCompiler.compileMxmlFile(file, 
+                        new File(swfDir,file.name.replaceAll("mxml\$","swf")),
+                        true,appXml.type,appName)
+            }catch(Exception ex) {
+                println "error during compilation " + ex.getMessage()
+            }
+            println file.name + " compilation ended at: " + new Date()
+        }
+    }
+    
+    private static def checkXmlList(appXmlList,file) {
+        if(file.name.endsWith(".mxml")) {
+            String content = file.text
+            if(content.indexOf("</mx:Application>") != -1 ) {
+                appXmlList.add([file:file,type:WebCompilerType.application])
+            }else if(content.indexOf("</mx:Module>")!=-1){
+                appXmlList.add([file:file,type:WebCompilerType.application])
+            }
+        }
+    }
+    
+    
+}
