a59e9f353f37b0ff45d2cea36a6a4485dd89fe94
wdrai <wdrai@832c1c66-4827-0410-8465-ccb17913cd09>
2011-01-20 03:36:17 +0000

    Upgrade to 2.2.SP1

--- GdsflexGrailsPlugin.groovy
+++ GdsflexGrailsPlugin.groovy
@@ -1,24 +1,24 @@
-import org.codehaus.groovy.grails.web.context.ServletContextHolder;
-
 /*
   GRANITE DATA SERVICES
-  Copyright (C) 2009 ADEQUATE SYSTEMS SARL
+  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
 
   This file is part of Granite Data Services.
 
   Granite Data Services is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or (at your
+  it under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
   option) any later version.
- 
+
   Granite Data Services is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
   for more details.
- 
-  You should have received a copy of the GNU Lesser General Public License
+
+  You should have received a copy of the GNU Library General Public License
   along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
+
+import org.codehaus.groovy.grails.web.context.ServletContextHolder;
 
 import java.util.concurrent.*
 import org.springframework.orm.hibernate3.AbstractSessionFactoryBean
@@ -33,7 +33,7 @@ import grails.util.BuildSettings
 
 
 class GdsflexGrailsPlugin {
-    def version = "0.8.4"
+    def version = "0.8.5"
     def author = "William Drai, Ford Guo"
     def authorEmail = "william.drai@graniteds.org"
     def title = "Integration between Grails and GraniteDS/Flex"
